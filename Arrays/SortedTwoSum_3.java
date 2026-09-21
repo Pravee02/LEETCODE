a83fdaf6bd0d64a59e90bc5c313be71b5ed56f25
Praveen M <praveenmpravee7242@gmail.com>
2026-09-21 20:13:48 +0530

    new file

--- Arrays/SortedTwoSum_3.java
+++ Arrays/SortedTwoSum_3.java
@@ -1,16 +1,36 @@
-// public class SortedTwoSum_3 {
-//     static int removeDuplicates(int[] numbers)
-//     {
-//         int right = 0;
-//         int left = 1;
-//         int count = 0;
-//         while(left<numbers.length)
-//         {
-//             if(numbers[right] == numbers[left])
-//             {
-//                 count += 1; 
-//             }
-//         }
-//     }
+public class SortedTwoSum_3 {
+    static int removeDuplicates(int[] numbers)
+    {
+        int right = 0;
+        int left = 1;
+       
+       int count = 1;
+
+        while(right < numbers.length-1)
+        {
+            if(numbers[right] == numbers[left])
+            {
+                                                      
+                right++;
+                
+                left++;
+            }
+            else {
+                count += 1;
+                right++;
+                left++;
+                                                                      
+            }
+        }
+                                                               
+
+        return count;
+    }
+    public static void main(String[] args) {
+        
+        int num[] = {1, 2, 3, 4, 5};
+        int result = removeDuplicates(num);
+        System.out.println(result);
+    }
     
-// }
+}
